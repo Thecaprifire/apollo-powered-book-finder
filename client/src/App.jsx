2b68587248b218e8b5b027a1dec85d45d1cb140b
Ian Daluz <janiandaluz@yahoo.com>
2024-09-18 21:02:58 -0600

    created Apollo Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,38 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
 
+// Creating an httpLink for GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Set up authentication headers with setContext
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Creating Apollo Client with authLink and httpLink
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
