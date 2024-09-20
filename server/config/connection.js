3801c28e9dcae1f59ef4678c9afdc17c73f73e42
Ian Daluz <janiandaluz@yahoo.com>
2024-09-20 10:07:40 -0600

    modified file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// Remove deprecated options for Mongoose 6+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
